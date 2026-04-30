fc569a1bc9f27555108755b7d407f9a35bc85232
김민재 <kim400@gimminjaeui-MacBookPro.local>
2026-04-30 12:47:06 +0900

    add

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/pb/:path*',
+        destination: `${process.env.POCKETBASE_URL || 'http://pocketbase:8090'}/:path*`,
+      },
+    ]
+  },
 };
 
 export default withNextIntl(nextConfig);
